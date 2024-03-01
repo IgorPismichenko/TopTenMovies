0b5c52dea3f54d7dc75022ebca3d1f78664752c5
Pismichenko.i <pismichenko.i@gmail.com>
2024-03-01 11:37:10 +0200

    #Законченный проект

--- TopTenMovies/Controllers/HomeController.cs
+++ TopTenMovies/Controllers/HomeController.cs
@@ -1,22 +1,160 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using System.IO;
 using TopTenMovies.Models;
 
 namespace TopTenMovies.Controllers
 {
     public class HomeController : Controller
     {
-        MoviesContext db;
-        public HomeController(MoviesContext context)
+        private readonly MoviesContext db;
+        IWebHostEnvironment _appEnvironment;
+        public HomeController(MoviesContext context, IWebHostEnvironment appEnvironment)
         {
             db = context;
+            _appEnvironment = appEnvironment;
         }
 
         public async Task<IActionResult> Index()
         {
-            IEnumerable<Movie> movies = await Task.Run(() => db.Movies);
-            ViewBag.Movies = movies;
+            return View(await db.Movies.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await db.Movies
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        public IActionResult Create()
+        {
             return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Director,Genre, Year,Poster,Description,Country,Budget,Box_Office,Duration")] Movie movie, IFormFile uploadedFile)
+        {
+            if (ModelState.IsValid)
+            {
+                if (uploadedFile != null)
+                {
+                    string path = "/Posters/" + uploadedFile.FileName;
+                    using (var fileStream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))
+                    {
+                        await uploadedFile.CopyToAsync(fileStream);
+                    }
+                    movie.Poster = path;
+                }
+                db.Add(movie);
+                await db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movie);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await db.Movies
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var movie = await db.Movies.FindAsync(id);
+            if (movie != null)
+            {
+                db.Movies.Remove(movie);
+            }
+
+            await db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await db.Movies.FindAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            return View(movie);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Director,Genre, Year,Poster,Description,Country,Budget,Box_Office,Duration")] Movie movie, IFormFile uploadedFile)
+        {
+            if (id != movie.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (uploadedFile != null)
+                    {
+                        string path = "/Posters/" + uploadedFile.FileName;
+                        using (var fileStream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))
+                        {
+                            await uploadedFile.CopyToAsync(fileStream);
+                        }
+                        movie.Poster = path;
+                    }
+                    db.Update(movie);
+                    await db.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MovieExists(movie.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movie);
+        }
+
+        private bool MovieExists(int id)
+        {
+            return db.Movies.Any(e => e.Id == id);
         }
     }
 }
--- TopTenMovies/Models/Movie.cs
+++ TopTenMovies/Models/Movie.cs
@@ -1,13 +1,39 @@
-﻿namespace TopTenMovies.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TopTenMovies.Models
 {
     public class Movie
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Поле має бути встановлено")]
+        [Display(Name = "Назва")]
         public string? Title { get; set; }
+        [Required(ErrorMessage = "Поле має бути встановлено")]
+        [Display(Name = "Режисер")]
         public string? Director { get; set; }
+        [Required(ErrorMessage = "Поле має бути встановлено")]
+        [Display(Name = "Жанр")]
         public string? Genre { get; set; }
+        [Required(ErrorMessage = "Поле має бути встановлено")]
+        [Display(Name = "Рік")]
+        [Range(1910, 2024, ErrorMessage = "Недопустимий рік")]
         public int Year { get; set;}
+        [Display(Name = "Постер")]
         public string? Poster { get; set; }
+        [Required(ErrorMessage = "Поле має бути встановлено")]
+        [Display(Name = "Опис")]
         public string? Description { get; set; }
+        [Required(ErrorMessage = "Поле має бути встановлено")]
+        [Display(Name = "Країна")]
+        public string? Country { get; set; }
+        [Required(ErrorMessage = "Поле має бути встановлено")]
+        [Display(Name = "Бюджет")]
+        public string? Budget { get; set; }
+        [Required(ErrorMessage = "Поле має бути встановлено")]
+        [Display(Name = "Збори")]
+        public string? Box_Office { get; set; }
+        [Required(ErrorMessage = "Поле має бути встановлено")]
+        [Display(Name = "Тривалість")]
+        public string? Duration { get; set; }
     }
 }
